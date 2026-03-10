a0cddf79617206243beb9c0e35ec544893676e61
Dick BkD <bkd@hoornbeeck.nl>
2026-03-10 21:53:40 +0100

    Brainstorm code and assets

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
-<script setup></script>
+<script setup>
+import DashBoard from './components/DashBoard.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <header>
+    <img alt="REC logo" class="logo" src="./assets/logo_white_black.svg" width="202" height="46" />
+    <div class="wrapper">
+      <DashBoard msg="RealEstateCare Project!" />
+    </div>
+  </header>
+  <h1>Tekst uit App.vue</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+  border-radius: 8px;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
